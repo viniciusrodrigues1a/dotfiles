a2d746908897dfad769921eeb04e879f88ba8a21
Vinícius Rodrigues de Aro <viniciusrodrigues.aro@gmail.com>
2024-07-27 15:49:52 -0300

    add catppuccin theme for neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,14 @@
 return {
-  {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd([[colorscheme nightfly]])
-    end,
-  },
+	{
+		"bluz71/vim-nightfly-guicolors",
+		priority = 1000, -- make sure to load this before all the other start plugins
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
